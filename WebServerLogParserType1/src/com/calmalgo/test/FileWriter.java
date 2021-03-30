277b1b285541b51ea93891ca81722392f366dc88
Horizon Gallery Coding Research <69739029+hgallery@users.noreply.github.com>
2021-03-29 22:17:09 -0400

    Update FileWriter.java

--- WebServerLogParserType1/src/com/calmalgo/test/FileWriter.java
+++ WebServerLogParserType1/src/com/calmalgo/test/FileWriter.java
@@ -2,8 +2,8 @@ package com.calmalgo.test;
 
 
 /**
- Both Parser and Statistics write data to the corresponding file.
- Because of that both of them implement FileWriter.
+ This interface is implemented by Parser and Statistics as both of them write to the corresponding data file
+ in their own way. Potentially additional classes that write to a data file can implement this interface.
  **/
 interface FileWriter {
     void writeFile();
