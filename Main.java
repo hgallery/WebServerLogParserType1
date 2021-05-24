a71e7c1612e3568b9042daebb6f4da74aa983656
Horizon Gallery Coding Research <info.calmalgo@gmail.com>
2021-05-23 22:52:48 -0400

    Add files via upload
    All files in the same folder

---
+++ Main.java
@@ -0,0 +1,45 @@
+package com.calmalgo.test;
+import java.util.List;
+
+
+/**
+ The Main class uses the Parser class and Statistics class.
+ **/
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        var parser = new Parser();
+        parser.extractListOfUsefulItems();
+
+
+        String titleUsefulItems = """
+                                
+                                Useful Items
+                                -------------------
+                                """;
+        System.out.print(titleUsefulItems);
+        parser.getListOfUsefulItems().forEach(System.out::println);
+        parser.writeFile();
+
+
+
+        List<List<String>> listOfUsefulItems = parser.getListOfUsefulItems();
+        var statistics = new Statistics(listOfUsefulItems);
+        statistics.sumUpUsageVolume();
+
+
+        String titleUsageVolume = """
+                                 
+                                 Usage Volume
+                                 -------------------
+                                 """;
+        System.out.print(titleUsageVolume);
+        statistics.getUsageVolume().forEach( (k,v) -> System.out.println(k + " " + v));
+        statistics.writeFile();
+
+
+    }
+}
+
---
+++ Statistics.java
@@ -0,0 +1,68 @@
+package com.calmalgo.test;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+
+/**
+ This class gets a list of useful data items, tallies the food order usage, and writes to a file.
+ **/
+class Statistics implements FileWriter{
+
+    private final List<List<String>> listOfUsefulItems;
+    private final Map<String, Integer> usageVolume;
+
+    Statistics (List<List<String>> listOfUsefulItems) {
+        this.listOfUsefulItems = listOfUsefulItems;
+
+        this.usageVolume = new TreeMap<>();
+        for (String foodCode : Constants.foodCodes)
+            usageVolume.put(foodCode, 0);
+    }
+
+    void sumUpUsageVolume() {
+        for (List<String> usefulItems : listOfUsefulItems) {
+            for (String foodCode : Constants.foodCodes) {
+                if (usefulItems.contains(foodCode)) {
+                    int tally = usageVolume.get(foodCode);
+                    usageVolume.put (foodCode, ++tally);
+                }
+            }
+        }
+    }
+
+
+
+
+    @Override
+    public void writeFile() {
+
+        Path outputPath = Path.of(Constants.OUTPUT_USAGE_VOLUME_FILE_NAME);
+
+        try (BufferedWriter writer = Files.newBufferedWriter(outputPath)) {
+
+            if (!Files.exists(outputPath))
+                Files.createFile(outputPath);
+
+            for (Map.Entry<String, Integer> entry : usageVolume.entrySet()) {
+                writer.write(entry.getKey() + " " + entry.getValue());
+                writer.newLine();
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+   }
+
+
+    Map<String, Integer> getUsageVolume() {
+        return usageVolume;
+    }
+
+
+}
